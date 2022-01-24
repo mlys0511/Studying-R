2dbc0d42ec789db7a19ca40719a88ab55537d107
YunSung Kang <kkangyun0511@naver.com>
2022-01-24 14:52:18 +0900

    init studying R D+12

---
+++ 22.01.24/studying-R12.R
@@ -0,0 +1,20 @@
+library(dplyr)
+df<-data.frame(sex=c("M","F",NA,"M","F"),score=c(5,4,3,4,NA))
+df
+is.na(df)
+table(is.na(df))
+table(is.na(df$sex))
+table(is.na(df$score))
+mean(df$score)
+sum(df$score)
+df%>%filter(is.na(score))
+df%>%filter(!is.na(score))
+df_nomiss<-df%>%filter(!is.na(score))
+mean(df_nomiss$score)
+sum(df_nomiss$score)
+df_nomiss<-df%>%filter(!is.na(score)&!is.na(sex))
+df_nomiss
+df_nomiss2<-na.omit(df)
+df_nomiss2
+mean(df$score,na.rm = T)
+sum(df$score,na.rm = T)
