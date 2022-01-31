7f8a5492a1beb67f209e9d0727507df21d3087c9
YunSung Kang <kkangyun0511@naver.com>
2022-01-31 09:57:24 +0900

    init studying R D+18

---
+++ 22.01.31/studying_R18.R
@@ -0,0 +1,18 @@
+library(foreign)
+library(dplyr)
+library(ggplot2)
+library(readxl)
+welfare<-read.spss(file = "C:/Users/kkang/Desktop/github/Studying-R/Koweps_hpc10_2015_beta1.sav",to.data.frame=T)
+welfare<-rename(welfare,sex=h10_g3,birth=h10_g4,marriage=h10_g10,religion=h10_g11,income=p1002_8aq1,code_job=h10_eco9,code_region=h10_reg7)
+summary(welfare$birth)
+qplot(welfare$birth)
+table(is.na(welfare$birth))
+welfare$birth<-ifelse(welfare$birth==9999,NA,welfare$birth)
+table(is.na(welfare$birth))
+welfare$age<-2015-welfare$birth+1
+summary(welfare$age)
+qplot(welfare$age)
+age_income<-welfare%>%filter(!is.na(income))%>%group_by(age)%>%summarise(mean_income=mean(income))
+head(age_income)
+ggplot(data=age_income,aes(x=age,y=mean_income))+geom_line()
+
