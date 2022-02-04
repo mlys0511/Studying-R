45119120e88c53c74f7bbdacec1ec1ff876ab4d8
YunSung Kang <kkangyun0511@naver.com>
2022-02-04 09:21:26 +0900

    init studying R D+22

---
+++ 22.02.04/studying_R22.R
@@ -0,0 +1,31 @@
+library(foreign)
+library(dplyr)
+library(ggplot2)
+library(readxl)
+welfare<-read.spss(file = "C:/Users/kkang/Desktop/github/Studying-R/Koweps_hpc10_2015_beta1.sav",to.data.frame=T)
+welfare<-rename(welfare,sex=h10_g3,birth=h10_g4,marriage=h10_g10,religion=h10_g11,income=p1002_8aq1,code_job=h10_eco9,code_region=h10_reg7)
+table(welfare$religion)
+welfare$religion<-ifelse(welfare$religion==1,"yes","no")
+table(welfare$religion)
+qplot(welfare$religion)
+table(welfare$marriage)
+welfare$group_marriage<-ifelse(welfare$marriage==1,"marriage",ifelse(welfare$marriage==3,"divorce",NA))
+table(welfare$group_marriage)
+table(is.na(welfare$group_marriage))
+qplot(welfare$group_marriage)
+religion_marriage<-welfare%>%filter(!is.na(group_marriage))%>%count(religion,group_marriage)%>%group_by(religion)%>%mutate(pct=round(n/sum(n)*100,1))
+religion_marriage
+divorce<-religion_marriage%>%filter(group_marriage=="divorce")%>%select(religion,pct)
+divorce
+ggplot(data=divorce,aes(x=religion,y=pct))+geom_col()
+welfare$age<-2015-welfare$birth+1
+welfare<-welfare%>%mutate(ageg=ifelse(age<30,"young",ifelse(age<=59,"middle","old")))
+ageg_marriage<-welfare%>%filter(!is.na(group_marriage))%>%count(ageg,group_marriage)%>%group_by(ageg)%>%mutate(pct=round(n/sum(n)*100,1))
+ageg_divorce<-ageg_marriage%>%filter(ageg!="young"&group_marriage=="divorce")%>%select(ageg,pct)
+ageg_divorce
+ggplot(data=ageg_divorce,aes(x=ageg,y=pct))+geom_col()
+ageg_religion_marriage<-welfare%>%filter(!is.na(group_marriage)&ageg!="young")%>%count(ageg,religion,group_marriage)%>%group_by(ageg,religion)%>%mutate(pct=round(n/sum(n)*100,1))
+ageg_religion_marriage
+df_divorce<-ageg_religion_marriage%>%filter(group_marriage=="divorce")%>%select(ageg,religion,pct)
+df_divorce
+ggplot(data=df_divorce,aes(x=ageg,y=pct,fill=religion))+geom_col(position = "dodge")
